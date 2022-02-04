8178ff64677e6a0e75d880833fc6449b0fc68d63
LoopFruits <Kensworldc@gmail.com>
2022-02-04 10:41:27 -0500

    commit tree 631abd48b8c486be5139dc2e1b3f76a197c1db4c

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function destructivelyAppendCat(Ralph){
 
 function destructivelyPrependCat(Bob){
     cats.unshift(Bob);
+    console.log('result:', Bob)
 }
 
 function destructivelyRemoveLastCat(){
@@ -20,4 +21,33 @@ function destructivelyRemoveFirstCat(){
     console.log('result', cats);
 }
 
+// (append) add a name to the end of the array and return a new array, leaving the OG unchanged
 
+function appendCat() {
+    const copyCats = [...cats, 'Broom']
+    return copyCats;
+}
+
+
+//they want me to remove the first cat from the cats array(prepend) and return a new array 
+
+function prependCat(){
+    console.log('line26:', cats);
+    const newCats = ['Arnold', ...cats]
+    return newCats
+}
+
+
+//re move the last cat in the cats array and retunr a new array, leaving the cats array unchanged 
+
+function removeLastCat(){
+    console.log('start: ', cats)
+    const removeLastCat = cats.slice(0,-1);
+    console.log('end: ', removeLastCat)
+    return removeLastCat
+}
+
+function removeFirstCat(){
+    const catsCats = cats.slice(1)
+    return catsCats
+}
